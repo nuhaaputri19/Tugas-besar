ca2432953372ddf1441718587bdf25780abf177e
Yaasmiin Nuhaa <92882385+nuhaaputri19@users.noreply.github.com>
2023-04-07 00:56:15 +0700

    Add files via upload

---
+++ hal/config/db/koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+// informasi untuk koneksi ke database
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "tugas_planner";
+$conn = mysql_connect($host,$user,$password,$dbname);
+?>
